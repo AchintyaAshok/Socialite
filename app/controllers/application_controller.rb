f53abef70d29cb81c4ec7ea044f6f507dfa1cc79
Vincent <vfong01@gmail.com>
2014-11-17 14:07:39 -0500

    added comments to yelp api calls
    added comments in application_controller.rb to assist aarti

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,25 +3,30 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  $client = Yelp::Client.new({consumer_key: 'P0c0K1K4r1BQL35xHG55Dg',
-                    		  consumer_secret: 'x1aO6bg8TLD99-iP9bqnQBlstio',
-                    		  token: 'pZFBgc9FwJel-Cf8ObUw5Lzij_0YRr_M',
-                    		  token_secret: 'ivpsd0yGZmWhvolew_0LmHwEBvE'
-                  	 	    })
+  $yelpClient = Yelp::Client.new({consumer_key: 'P0c0K1K4r1BQL35xHG55Dg',
+                    		          consumer_secret: 'x1aO6bg8TLD99-iP9bqnQBlstio',
+                    		          token: 'pZFBgc9FwJel-Cf8ObUw5Lzij_0YRr_M',
+                    		          token_secret: 'ivpsd0yGZmWhvolew_0LmHwEBvE'
+                  	 	          })
 
+  #yelp/search/:lat/:long, returns buisnesses in the area specified by lat and long
   def searchByLatLong
-  	render json: $client.search_by_coordinates({latitude: params[:lat], longitude: params[:long]}, {limit: 5}).businesses
+  	render json: $yelpClient.search_by_coordinates({latitude: params[:lat], longitude: params[:long]}, {limit: 5}).businesses
   end
 
+  #yelp/search/:id, return a buisness object give the buisness id which can be pulled off the JSON from above query
   def searchById
-  	render json: $client.business(params[:id])
+  	render json: $yelpClient.business(params[:id])
   end
 
+  #yelp/searchExact/:exactLocation, if user enters phrases, zip code, exact address, etc
   def searchByExact
-  	render json: $client.search(params[:exactLocation], {limit: 5})
+  	render json: $yelpClient.search(params[:exactLocation], {limit: 5})
   end
 
+  #yelp/searchTerm/:term/:lat/:long, returns buisnessed that match a certain term, ex: "movies" near a specific lat long
   def searchByTerm
-  	render json: $client.search_by_coordinates({latitude: params[:lat], longitude: params[:long]}, {limit: 5, term: params[:term]}).businesses
+  	render json: $yelpClient.search_by_coordinates({latitude: params[:lat], longitude: params[:long]}, {limit: 5, term: params[:term]}).businesses
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   
   get 'yelp/searchExact/:exactLocation' => 'application#searchByExact'
   get 'yelp/searchTerm/:term/:lat/:long' => 'application#searchByTerm', :lat => /.*/, :long => /.*/
-
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
