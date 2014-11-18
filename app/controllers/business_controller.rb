7fb33b3cfb752c851b7eaae7fe7c87846d92f723
abhishek7 <abhishek.sharma@nyu.edu>
2014-11-18 17:13:35 -0500

    Business Controller
    Added prototypes for each, adding actual functional code later tonight

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -4,4 +4,26 @@ class BusinessController < ApplicationController
 		render json: @business
 	end
 
+	def findEventsHostedByBusinesses
+		# Code to get business id
+
+		# Code for using the id passed as a parameter
+
+		# Return events
+	end
+
+	def findRegisteredBusinesses
+		# Get business id, or business name
+
+		# Return business name
+	end
+
+	def createEvent
+
+		# get information from text fields about events
+
+		# input into database so it saves and reflects
+
+	end
+
 end
