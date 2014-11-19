b79b75917eec5090eb97926497f02251b7404a5d
Vincent <vfong01@gmail.com>
2014-11-19 01:47:12 -0500

    fixes db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20141117225901) do
-=======
 ActiveRecord::Schema.define(version: 20141119031735) do
->>>>>>> origin/be_master
 
   create_table "events", force: true do |t|
     t.datetime "created_at"
