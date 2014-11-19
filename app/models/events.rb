2ca2f20a5ca22e8fee99f1cc96e0bcba9143b4ef
Achintya Ashok <achintyaashok@Aurora.local>
2014-11-18 23:21:51 -0500

    Finally was able to fix all the associations between users, events and user_events. Then added seed data for users to see all the events they are going to or are following.

--- app/models/events.rb
+++ app/models/events.rb
@@ -1,2 +1,4 @@
 class Events < ActiveRecord::Base
+	has_many :user_events, :class_name => 'UserEvents', :dependent => :destroy
+	has_many :users, :through => :user_events
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -100,3 +100,41 @@ vincent = Users.create!(
 	email: "vincenzo@socialite.com"
 )
 vincent.save!
+
+# User Events -- Events that users have chosen to follow or join:
+UserEvents.create(
+	users_id: Users.find(aarti.id),
+	events_id: Events.find(e1.id),
+	following: true,
+	going: true
+)
+UserEvents.create(
+	users_id: Users.find(aarti.id),
+	events_id: Events.find(e2.id),
+	following: true,
+	going: true
+)
+UserEvents.create(
+	users_id: Users.find(achintya.id),
+	events_id: Events.find(e3.id),
+	following: true,
+	going: false
+)
+UserEvents.create(
+	users_id: Users.find(achintya.id),
+	events_id: Events.find(e4.id),
+	following: true,
+	going: true
+)
+UserEvents.create(
+	users_id: Users.find(abhishek.id),
+	events_id: Events.find(e5.id),
+	following: true,
+	going: false
+)
+UserEvents.create(
+	users_id: Users.find(abhishek.id),
+	events_id: Events.find(e6.id),
+	following: true,
+	going: false
+)
