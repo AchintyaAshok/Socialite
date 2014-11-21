fdcedbf743175bfd50104a0f7c6bec0880e75c34
abhishek7 <abhishek.sharma@nyu.edu>
2014-11-21 14:02:46 -0500

    Added my own stuff

---
+++ db/migrate/20141120234818_create_businesses.rb
@@ -0,0 +1,12 @@
+class CreateBusinesses < ActiveRecord::Migration
+  def change
+    create_table :businesses do |t|
+
+      t.timestamps
+      t.string :name, null: false
+      t.string :number, null: false
+      t.string :email, null: false
+      t.string :address, null: false
+    end
+  end
+end
